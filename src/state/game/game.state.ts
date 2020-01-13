f0926f00ea99dcbe30f856228b9e730150b9d307
Sascha Klatt <coding@klattiation.de>
2020-01-13 11:29:55 +0100

    remove 10 cards limit

--- src/state/game/game.state.ts
+++ src/state/game/game.state.ts
@@ -727,9 +727,7 @@ const resolveIds = (cards: CriteriaAssignment[]) => {
 
 export const AUDIENCE = resolveIds(AUDIENCE_CONFIGS)
 
-export const CARDS = (resolveIds(CARD_CONFIGS) as List<ResolvedCard>).filter(
-  (v, i) => i < 10
-)
+export const CARDS = resolveIds(CARD_CONFIGS) as List<ResolvedCard>
 
 export const initialState: GameState = Object.freeze({
   cardIndex: 0,
